9a480493632fc1960bfdebc33cd870370453c12a
mubashirAlgoace <mubashir.siddiqui@algoace.com>
2023-09-29 21:50:23 +0500

    section 2 complete

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -5,7 +5,7 @@ export default function Card(props: ICard) {
   const { heading, subHeading, content, linkText } = props;
   return (
     <div className="bg-white w-96 m-3 p-8 h-96 border-1 rounded-md dark:bg-black-mode ">
-      <h1 className="text-2xl font-bold text-gray-dark dark:text-light py-4">
+      <h1 className="text-3xl font-bold text-gray-dark dark:text-light py-4">
         {heading}
       </h1>
       <div className="flex flex-col justify-between items-start h-3/4">
@@ -15,7 +15,7 @@ export default function Card(props: ICard) {
           </h4>
           <p className="text-gray-light dark:text-light text-lg">{content}</p>
         </div>
-        <button className="flex">
+        <button className="flex items-center">
           {linkText} <RightFilledArrow />{" "}
         </button>
       </div>
